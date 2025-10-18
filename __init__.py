08c2eb732ce5b3b21b0f1ef24e6bf700416255d2
ZhiYu <144018856+DNPMBHC@users.noreply.github.com>
2025-10-19 01:14:10 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,14 +1,11 @@
 # -*- coding: utf-8 -*-
 
 from .get_image_name import GetImageNameNode
-from .save_label_to_txt import SaveLabelToTxtNode
 
 NODE_CLASS_MAPPINGS = {
     "GetImageNameNode": GetImageNameNode,
-    "SaveLabelToTxtNode": SaveLabelToTxtNode,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
     "GetImageNameNode": "获取图片名称",
-    "SaveLabelToTxtNode": "保存标签到TXT",
 }
