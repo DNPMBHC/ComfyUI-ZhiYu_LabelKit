6ae9b2f7b77d3ca43af2deb777698aa8c08b8372
ZhiYu <144018856+DNPMBHC@users.noreply.github.com>
2025-10-18 17:25:56 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,11 +1,30 @@
-# ZhiYu/__init__.py
-from . import batch_image_loader
-from . import save_label_to_txt
+# __init__.py
+# -*- coding: utf-8 -*-
+"""
+ZhiYu_LabelKit - ComfyUI 自定义节点包
+节点分类: ZhiYu/工具箱
+包含：
+- 批量加载图片
+- 获取图片名称
+- 格式转换
+- 保存标签到 TXT
+"""
 
-NODE_CLASS_MAPPINGS = {}
-NODE_CLASS_MAPPINGS.update(batch_image_loader.NODE_CLASS_MAPPINGS)
-NODE_CLASS_MAPPINGS.update(save_label_to_txt.NODE_CLASS_MAPPINGS)
+from .batch_image_loader import BatchImageLoader
+from .get_image_name import GetImageName
+from .format_converter import FormatConverter
+from .save_label_to_txt import SaveLabelToTxt
 
-NODE_DISPLAY_NAME_MAPPINGS = {}
-NODE_DISPLAY_NAME_MAPPINGS.update(batch_image_loader.NODE_DISPLAY_NAME_MAPPINGS)
-NODE_DISPLAY_NAME_MAPPINGS.update(save_label_to_txt.NODE_DISPLAY_NAME_MAPPINGS)
+NODE_CLASS_MAPPINGS = {
+    "BatchImageLoader": BatchImageLoader,
+    "GetImageName": GetImageName,
+    "FormatConverter": FormatConverter,
+    "SaveLabelToTxt": SaveLabelToTxt,
+}
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "BatchImageLoader": "批量加载图片",
+    "GetImageName": "获取图片名称",
+    "FormatConverter": "格式转换",
+    "SaveLabelToTxt": "保存标签到TXT",
+}
