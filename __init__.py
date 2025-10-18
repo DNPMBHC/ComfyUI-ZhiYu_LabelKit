95c3f898e0e76730842a55078681afca92fce89d
ZhiYu <144018856+DNPMBHC@users.noreply.github.com>
2025-10-18 16:52:20 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,6 +1,11 @@
-from .save_label_to_txt import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
+# ZhiYu/__init__.py
+from . import batch_image_loader
+from . import save_label_to_txt
 
-__all__ = [
-    "NODE_CLASS_MAPPINGS",
-    "NODE_DISPLAY_NAME_MAPPINGS",
-]
+NODE_CLASS_MAPPINGS = {}
+NODE_CLASS_MAPPINGS.update(batch_image_loader.NODE_CLASS_MAPPINGS)
+NODE_CLASS_MAPPINGS.update(save_label_to_txt.NODE_CLASS_MAPPINGS)
+
+NODE_DISPLAY_NAME_MAPPINGS = {}
+NODE_DISPLAY_NAME_MAPPINGS.update(batch_image_loader.NODE_DISPLAY_NAME_MAPPINGS)
+NODE_DISPLAY_NAME_MAPPINGS.update(save_label_to_txt.NODE_DISPLAY_NAME_MAPPINGS)
