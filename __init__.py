1ef5c186ccb6b96a8c1e7f38a32805ef1e1cb0d2
ZhiYu <144018856+DNPMBHC@users.noreply.github.com>
2025-10-19 12:44:50 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,12 +1,17 @@
-# -*- coding: utf-8 -*-
-
-# 导入主类（其他类可选导入）
-from .batch_image_loader import BatchLoadImagesWithNames
+from .batch_image_loader import (
+    BatchLoadImagesWithNames,
+    LoadImagesFromDirList,  # 等
+    # ... 其他
+)
 
 NODE_CLASS_MAPPINGS = {
     "BatchLoadImagesWithNames": BatchLoadImagesWithNames,
+    "LoadImagesFromDirList": LoadImagesFromDirList,
+    # ... 更多键
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
     "BatchLoadImagesWithNames": "批量加载图片并提取名称",
+    "LoadImagesFromDirList": "列表加载目录图片",
+    # ... 更多，建议用中文
 }
